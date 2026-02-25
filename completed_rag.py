64a90e4a2b9dc826d8613debb9f3cd19e3ba5864
JClinton20 <157168898+JClinton20@users.noreply.github.com>
2026-02-25 09:53:17 -0400

    Added FastAPI routes/endpoints

--- completed_rag.py
+++ completed_rag.py
@@ -7,7 +7,6 @@ from supabase import create_client
 from query_data import query_rag
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import SupabaseVectorStore
-# from google import genai
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
