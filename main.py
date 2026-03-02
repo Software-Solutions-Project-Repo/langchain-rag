139188a384ff6644323016a8bf307221b7e03516
SincerelyKM <keddishamcintyre@gmail.com>
2026-03-02 14:27:07 -0400

    Added endpoint

--- main.py
+++ main.py
@@ -11,10 +11,13 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from dotenv import load_dotenv
 
+from LLM import ask_aichatbot_payroll_question
+
+
 load_dotenv()
 
 
-from completed_rag import invoke_LLM
+#from completed_rag import invoke_LLM
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 
@@ -32,6 +35,9 @@ llm = ChatGoogleGenerativeAI(
     temperature=0.3,
     google_api_key=os.getenv("GOOGLE_API_KEY"),
 )
+
+chat_history = []
+
 
 # ----------- Pydantic Models -----------
 
@@ -60,11 +66,66 @@ def root():
 
 # Post request endpoint for sending questions to Open WebUI
 @app.post("/v1/chat/completions")
-def chat(request: ChatRequest):
-    #For Keddish to complete
-    #Use invoke_LLM function to process user queries
+async def chat(request: ChatRequest):
+    try:
+        user_msg = next((m.content for m in reversed(request.messages)if m.role == "user"), None)
+        if not user_msg:
+            return {"error": "No user message found"}
 
-    return None # Change this
+        chat_history.append(f"User:{user_msg}")
+        
+       
+        def event_generator():
+            answer = ask_aichatbot_payroll_question(user_msg)
+            
+            chat_history.append(f"Assistant: {answer}")
+            for chunk in answer.split():
+                data = {
+                    "id": f"chatcml-{int(time.time())}", 
+                    "object": "chat.completion.chunk", 
+                    "choices": [
+                        {
+                        "delta": {"content": chunk + " "}, 
+                        "index": 0, 
+                          
+                        }
+                        ], 
+                    }
+                yield f"data: {json.dumps(data)}\n\n"
+            yield "data: [DONE]\n\n"
+
+          
+        
+        if request.stream is False: 
+            
+           answer = ask_aichatbot_payroll_question(user_msg)
+
+           return {
+             "id": f"chatcml-{int(time.time())}", 
+             "object": "chat.completion",
+             "created": int(time.time()), 
+             "model": "payroll-rag", 
+             "choices": [
+                {
+                  "index": 0, 
+                  "message": {
+                      "role": "assistant", 
+                      "content": answer
+                    }, 
+                   "finish_reason": "stop"
+               }
+            ], 
+        }
+        return StreamingResponse(
+                event_generator(), 
+                media_type = "text/event-stream", 
+                )
+    except Exception as e:
+        print("Error in chat completions route", e)
+        return{"error": str(e)}
+
+
+
 
 # This is required by Open WebUI to discover available models.
 @app.get("/v1/models")
