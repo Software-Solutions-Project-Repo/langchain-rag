11873a07f813a5d3322727a2fb91ff0a580c117f
funky473 <hsboxing19@gmail.com>
2026-03-03 21:17:13 -0400

    fix: update uvicorn run command to reference main module

--- main.py
+++ main.py
@@ -147,7 +147,7 @@ def list_models():
     
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 
 
