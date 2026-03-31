a4f8f3c66cab9cee9018cbea9b1386462a9fc91c
SincerelyKM <keddishamcintyre@gmail.com>
2026-03-31 13:44:32 -0400

    LLM can receive queries from the question bank and error code table now

--- query_data.py
+++ query_data.py
@@ -23,21 +23,64 @@ def query_rag(query_text: str):
     supabase = create_client(os.environ["SUPABASE_URL"], os.environ["SUPABASE_KEY"])
     embedding_function = get_embedding_function()
 
-    query_vector = embedding_function.embed_query(query_text)
-    response = supabase.rpc(
-        "match_documents",
-        {"query_embedding": query_vector, "match_count": 5},
+    try:
+
+       query_vector = embedding_function.embed_query(query_text)
+       response = supabase.rpc(
+           "match_documents",
+           {"query_embedding": query_vector, "match_count": 5},
+         ).execute()
+    except Exception as e:
+           print(f"Error calling match_documents: {e}")  
+
+    #This queries the question bank 
+    try:
+        question_response = supabase.rpc(
+        "match_qa",
+        {"query_embedding": query_vector, "match_count": 5, "filter" :{}
+         },
     ).execute()
+       
+    except Exception as e:
+        print(f"Error calling match_qa: {e}")
+    try:
+        if not question_response.data:
+            print("empty, retrying without filter")
+            question_response = supabase.rpc(
+            "match_qa",
+            {"query_embedding": query_vector, "match_count": 5, "filter" :{}
+            },
+            ).execute()
+      
+    except Exception as e:
+           print(f"Error calling match_qa: {e}")  
+            #This queries the Error code 
+
+    try:
+        error_code_response = supabase.rpc(
+            "match_error_code_qa",
+             {"query_embedding": query_vector, "match_count": 5, "filter":{}},
+             ).execute()
+        
+    except Exception as e:
+           print(f"Error calling match_error_code_qa: {e}")  
+    
+    try:
+        if not error_code_response.data:
+            print("empty, retrying without filter")
+            error_code_response = supabase.rpc(
+            "match_error_code_qa",
+            {"query_embedding": query_vector, "match_count": 5},
+            ).execute()
+       
+    except Exception as e:
+           print(f"Error calling match_error_code_qa: {e}")  
 # The response contains the matched documents along with their similarity scores. We print the results in a readable format.
-    results = response.data or []
+    results = (response.data or [] ) +(question_response.data or [])+(error_code_response.data or [])
     if not results:
         print("No results found.")
         return
-# Print the results with their similarity scores
-    #for i, row in enumerate(results, 1):
-       # print(f"\n--- Result {i} (similarity: {row.get('similarity', 'N/A'):.4f}) ---")
-       # print(row["content"])
-
+    
     return results
 
 
